82c47104065085055ccfb5acb2942a732f15aa00
gcca <git@gcca.dev>
2026-07-06 22:05:36 -0500

    20260706220536

--- cmd/urmom-create_minimal_user.lisp
+++ cmd/urmom-create_minimal_user.lisp
@@ -1,4 +1,5 @@
-(ql:quickload "unix-opts" :silent t)
+(ql:quickload :unix-opts :silent t)
+(ql:quickload :sqlite :silent t)
 
 (opts:define-opts
  (:name :help
@@ -13,28 +14,14 @@
         :arg-parser #'identity
         :required t)
 
- (:name :password
-        :description "Password for the new user"
-        :short #\p
-        :long "password"
-        :arg-parser #'identity
-        :required t)
-
  (:name :email
         :description "Email address for the new user"
         :short #\e
         :long "email"
-        :arg-parser #'identity)
-
- (:name :is-active
-        :description "Whether the user is active"
-        :short #\a
-        :long "is_active"
-        :type 'boolean
-        :default t))
+        :arg-parser #'identity))
 
 (defun print-help ()
-  (opts:describe :prefix "Create a new urmom user."
+  (opts:describe :prefix "Create a new urmom minimal user."
                  :usage-of "urmom-user_create.lisp"))
 
 (defun main ()
@@ -49,9 +36,6 @@
                                 (opts:exit 1)))
 
                        (format t
-                               "Creating user with the following options:~@
-                                Username: ~a~%Password: ~a~%Email: ~a~%Is Active: ~a~%"
+                               "Creating user with the following options:~%Username: ~a~%Email: ~a~%"
                                (getf options :username)
-                               (getf options :password)
-                               (getf options :email)
-                               (getf options :is-active))))
+                               (getf options :email))))
