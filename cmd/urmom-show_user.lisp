b14ed4074bc56b3dc7de6a329f57f9128fdf7147
gcca <git@gcca.dev>
2026-07-07 14:12:18 -0500

    20260707141218

--- cmd/urmom-show_user.lisp
+++ cmd/urmom-show_user.lisp
@@ -3,34 +3,29 @@
 (require :uiop)
 
 (defun usage ()
-  (format t "Usage: ~a <username>~%" (uiop:getenv "0")))
+  (format t "Usage: urmom-show_user <username>~%"))
 
 (defun main ()
   (let ((args (uiop:command-line-arguments)))
-    (format t "args: ~a~%" args)
-    (uiop:quit 0)
 
     (when (not (= (length args) 1))
       (usage)
       (uiop:quit 1))
 
     (sqlite:with-open-database
-     (conn "data/urmom.db")
-     (let ((item (sqlite:execute-to-list
-                  conn
-                  "SELECT
+        (conn "data/urmom.db")
+      (let ((item (sqlite:execute-to-list
+                   conn
+                   "SELECT
                      username,
                      email,
                      is_active,
                      created_at
                    FROM auth_user
-                   WHERE username = ?" (first args)))
+                   WHERE username = ?" (first args))))
 
-       (destructuring-bind (username email is-active created_at) (first item)
-         (format t "~11a ~a~%" "username:" username)
-         (format t "~11a ~a~%" "email:" email)
-         (format t "~11a ~a~%" "is-active:" is-active)
-         (format t "~11a ~a~%" "created_at:" created_at)))))
-
-    )
- )
+        (destructuring-bind (username email is-active created_at) (first item)
+          (format t "~11a ~a~%" "username:" username)
+          (format t "~11a ~a~%" "email:" email)
+          (format t "~11a ~a~%" "is-active:" is-active)
+          (format t "~11a ~a~%" "created_at:" created_at))))))
